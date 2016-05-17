c48dfb34a33eedb81bdde739b5ee6e5bd3c66ee1
pkhanna104 <pkhanna104@gmail.com>
2016-05-17 15:40:14 -0700

    make defaults com5, chan4

--- init/load_default.m
+++ init/load_default.m
@@ -15,12 +15,12 @@ set(handles.timeoutTime_box, 'String','1000');
 % set(handles.simNexusSource_pxx, 'Value',0);
 % set(handles.nexusSource, 'Value',1);
 % handles.neural_source_name.nexus = 1;
-set(handles.serial_port_box, 'String', 'COM3');
+set(handles.serial_port_box, 'String', 'COM5');
 
 % extractor params;
 set(handles.window_size_box,'String',400);
 set(handles.sampling_freq_box, 'String',422);
-set(handles.chan_idx_box, 'String',3);
+set(handles.chan_idx_box, 'String',4);
 %set(handles.diff_ref_box, 'String',0);
 
 % set(handles.low_frac_lim_box,'String',25);
@@ -31,6 +31,6 @@ set(handles.chan_idx_box, 'String',3);
  
 handles.extractor_params.width_t = 400;
 handles.extractor_params.fs = 422;
-handles.extractor_params.used_chan = 3;
+handles.extractor_params.used_chan = 4;
 handles.extractor_params.differential_chan = 2;
 handles.extractor_name = 'Nexus Power Ext.';
