0f22af4ac1dcf595c9da955f34a905b25c9f570e
pkhanna104 <pkhanna104@gmail.com>
2017-02-09 10:08:22 -0800

    added assertion statement

--- utils/rect_cell2mat.m
+++ utils/rect_cell2mat.m
@@ -4,9 +4,10 @@ function [rect_cell, rect_mat] = rect_cell2mat(cell, cell_shape)
     for ii = 1:length(cell)
         if size(cell{ii},1) ~= cell_shape(1)
             rect_cell{ii} = zeros(cell_shape)-1;
-            disp(strcat('Skipping ix: ', num2str(ii)));            
+            disp(strcat('Fixing ix: ', num2str(ii)));            
         else
             rect_cell{ii} = cell{ii};
         end
     end
     rect_mat  = cell2mat(rect_cell);
+    assert(size(rect_cell, 2) == size(rect_mat, 2))
